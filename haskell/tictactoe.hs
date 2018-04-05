c840111a5000c01b4562759fe32f8cd5c7eef00e
minhpro <minha0pro@gmail.com>
2018-04-05 08:15:26 +0700

    update tictactoe

--- haskell/tictactoe.hs
+++ haskell/tictactoe.hs
@@ -1,6 +1,7 @@
 import Data.Char
 import Data.List
 import System.IO
+import System.Random
 
 size :: Int
 size = 3
@@ -11,9 +12,9 @@ data Player = O | B | X
 
 type Grid = [[Player]]
 
-next :: Player -> Player
-next O = X
-next X = O
+nextPlayer :: Player -> Player
+nextPlayer O = X
+nextPlayer X = O
 
 --Grid utilities
 empty :: Grid
@@ -114,7 +115,7 @@ run' g p | wins O g = putStrLn "Player O wins!\n"
                 case move g i p of
                     []  -> do  putStrLn "ERROR: Invalid move"
                                run' g p
-                    [g'] -> do run g' (next p)  
+                    [g'] -> do run g' (nextPlayer p)  
                     
 prompt :: Player -> String
 prompt p = "Player " ++ show p ++ ", enter your move "
@@ -134,7 +135,7 @@ data Tree a = Node a [Tree a]
                     deriving Show
 
 gametree :: Grid -> Player -> Tree Grid
-gametree g p = Node g [gametree g' (next p) | g' <- moves g p]
+gametree g p = Node g [gametree g' (nextPlayer p) | g' <- moves g p]
 
 moves :: Grid -> Player -> [Grid]
 moves g p | won g = []
@@ -152,7 +153,7 @@ prune n (Node x ts) = Node x [prune (n-1) t | t <- ts]
 
 {-|
 Minimax algorithm
-Determine the best next move
+Determine the best nextPlayer move
 Labelling every node in the tree with a player value in the following manner:
     .Leaves are labelled with the wining player at this point if there is one, and
     the blank player otherwise
@@ -176,9 +177,78 @@ minimax (Node g ts)
                 ps = [p | Node (_,p) _ <- ts']
 
 depth :: Int
-depth = 5
+depth = 9
 
 -- the best move under the minimax algorith is given by moving to any grid 
 -- with the same label as the root.
 bestmove :: Grid -> Player -> Grid
-bestmove g p = head []
+bestmove g p = head [g' | Node (g',p') _ <- ts, p' == best]
+                    where
+                        tree = prune depth (gametree g p)
+                        Node (_,best) ts = minimax tree
+
+--Human vs computer
+-- main :: IO ()
+-- main = do 
+--         hSetBuffering stdout NoBuffering
+--         play empty O
+        
+play :: Grid -> Player -> IO ()
+play g p = do
+    cls
+    goto(1,1)
+    putGrid g 
+    play' g p
+
+-- play' :: Grid -> Player -> IO ()
+-- play' g p 
+--     | wins O g  = putStrLn "Player O wins!\n"
+--     | wins X g  = putStrLn "Player X wins!\n"
+--     | full g    = putStrLn "It's a draw!\n"
+--     | p == O    = do 
+--                     i <- getNat (prompt p)
+--                     case move g i p of
+--                         [] -> do putStrLn "ERROR: Invalid move"
+--                                  play' g p
+--                         [g'] -> play g' (nextPlayer p)
+--     | p == X    = do 
+--                     putStrLn "Player X is thinking.."
+--                     (play $! (bestmove g p)) (nextPlayer p)   
+
+--Choose a random move from the list of best moves
+--randomRIO :: (Int,Int) -> IO Int
+bestmoves :: Grid -> Player -> [Grid]
+bestmoves g p = [g' | Node (g',p') _ <- ts, p' == best]
+                    where
+                        tree = prune depth (gametree g p)
+                        Node (_,best) ts = minimax tree
+
+play' :: Grid -> Player -> IO ()
+play' g p 
+    | wins O g  = putStrLn "Player O wins!\n"
+    | wins X g  = putStrLn "Player X wins!\n"
+    | full g    = putStrLn "It's a draw!\n"
+    | p == O    = do 
+                    i <- getNat (prompt p)
+                    case move g i p of
+                        [] -> do putStrLn "ERROR: Invalid move"
+                                 play' g p
+                        [g'] -> play g' (nextPlayer p)
+    | p == X    = do 
+                    putStrLn "Player X is thinking.."
+                    i <- randomRIO(0,l-1)
+                    play (gs !! i) (nextPlayer p) 
+                        where 
+                            gs = bestmoves g p
+                            l = length gs
+                    
+--User decide to play first or second
+main :: IO ()
+main = do 
+        hSetBuffering stdout NoBuffering
+        putStr "Do you want to play first?(yes/no):"
+        answer <- getLine
+        if answer == "yes" then
+            play empty O
+        else
+            play empty X
